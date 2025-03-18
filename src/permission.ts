a3c09d5d43c8a310a497289a19e33896a7a67b66
huase84 <2569787692@qq.com>
2025-03-18 16:01:20 +0800

    修复全局前置路由守卫逻辑错误

--- src/permission.ts
+++ src/permission.ts
@@ -1,26 +1,17 @@
 import router from "@/router"
 import { userStore } from '@/store/user.ts'
 
-var isAuthenticated  = false
-
 // 前置路由守卫
-router.beforeEach((to, from) => {
+router.beforeEach(async (to, from) => {
   const user = userStore()
   // 判断用户是否登录
-  if(!isAuthenticated) {
-    if(to.name !== 'Login') {
-      // 获取用户信息
-      user.getUser().then(res => {
-        if(res.code == 200 && res.data != null) {
-          isAuthenticated = true
-        } else {
-          // 跳转至登录页 
-          return '/login'
-        }
-      }).catch(err => {
-        console.log(err)
-        return '/login'
-      })
+  if(to.name !== 'Login') {
+    // 获取用户信息
+    const res = await user.getUser()
+    if(res.code == 200 && res.data != null) {
+    } else {
+      // 跳转至登录页 
+      return "/login"
     }
   }
 })
