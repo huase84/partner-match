531a749495cdd6cd9ff028791123a2e75ec391a5
huase84 <2569787692@qq.com>
2025-03-18 16:03:41 +0800

    用户信息页添加获取用户信息和退出

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -8,35 +8,62 @@
   <van-cell title="电话" is-link :value="user.phone" @click="toEdit('phone', '电话', user.phone)" />
   <van-cell title="邮箱" is-link :value="user.email" @click="toEdit('email', '邮箱', user.email)" />
   <van-cell title="编号" :value="user.planetCode" />
-  <van-cell title="注册时间" :value="user.createTime.toISOString()" />
+  <van-cell title="注册时间" :value="user.createTime" />
+  <div style="margin: 16px;">
+    <van-button block type="primary" @click="logout">
+      退出
+    </van-button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { userStore } from '@/store/user';
 
 const router = useRouter();
 
-const user = {
-  id: 1,
-  username: '鱼皮',
-  userAccount: 'dogYupi',
+const user = ref({
+  id: '',
+  username: '',
+  userAccount: '',
   avatarUrl: '',
-  gender: '男',
-  phone: '12131133313',
-  email: '23432444@qq.com',
-  planetCode: '2220',
-  createTime: new Date(),
+  gender: '',
+  phone: '',
+  email: '',
+  planetCode: '',
+  createTime: '',
+})
+const userInfo = userStore();
+
+const getUserInfo = async () => {
+  userInfo.getUser().then((res) => {
+    if(res.code == 200) {
+      user.value = res.data
+      console.log(res.data, 'userInfo')
+    }
+  });
 };
+
+getUserInfo();
 
 const toEdit = (type: string, title: string, value: string) => {
   router.push({
     path: '/user/edit',
     query: {
-      id: user.id,
+      id: user.value.id,
       type: type,
       title: title,
       value: value,
     },
+  });
+}
+
+const logout = () => {
+  userInfo.userLogout().then((res) => {
+    if(res.code == 200) {
+      router.push('/login')
+    }
   });
 }
 
