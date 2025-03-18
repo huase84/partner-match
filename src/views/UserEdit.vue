25b9c5d09f740314c03b78b325f80c387bc647aa
huase84 <2569787692@qq.com>
2025-03-18 16:03:58 +0800

    添加用户更新

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -19,23 +19,35 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { showToast } from 'vant'
+import { updateUser } from '@/api/user';
 
 const route = useRoute();
+const router = useRouter();
 console.log(route.query);
 const value = ref();
 value.value = route.query == null ? null : route.query.value
 
 const onSubmit = (values) => {
   if(value.value == route.query.value) {
-    showToast.fail('请填写新的' + route.query.title)
+    showToast({
+      message: '请填写新的' + route.query.title,
+      position: 'bottom',
+    });
     return
   }
   var params = { 
     id: route.query.id,
     [route.query.type]: value.value
   };
-  console.log(params, values);
+  // 更新用户信息
+  updateUser(params).then(res => {
+    showToast({
+      message: '修改成功',
+      position: 'bottom',
+    });
+    router.back()
+  })
 };
 </script>
