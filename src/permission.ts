5f32519d35146411ae141e02dcf109fc33eba12a
huase84 <2569787692@qq.com>
2025-03-18 14:33:01 +0800

    添加全局路由前置守卫，校验用户登录控制页面跳转

---
+++ src/permission.ts
@@ -0,0 +1,26 @@
+import router from "@/router"
+import { useStore } from '@/store/user.ts'
+
+var isAuthenticated  = false
+
+// 前置路由守卫
+router.beforeEach((to, from) => {
+  const user = useStore()
+  // 判断用户是否登录
+  if(!isAuthenticated) {
+    if(to.name !== 'Login') {
+      // 获取用户信息
+      user.getUser().then(res => {
+        if(res.code == 200 && res.data != null) {
+          isAuthenticated = true
+        } else {
+          // 跳转至登录页 
+          return '/login'
+        }
+      }).catch(err => {
+        console.log(err)
+        return '/login'
+      })
+    }
+  }
+})
