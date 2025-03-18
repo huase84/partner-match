968ca7fda3a7982d6e11d113ed5fd6234fcc0b12
huase84 <2569787692@qq.com>
2025-03-18 14:30:48 +0800

    调整路由配置，将非登录页面放到子路由并统一引入layout组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import layout from "./layout/index.vue"
+// import layout from "./layout/index.vue"
 </script>
 
 <template>
-  <layout />
+  <RouterView />
+  <!-- <layout /> -->
 </template>
 
 <style scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,42 @@
 // 路由方法
 import { createWebHistory, createRouter } from 'vue-router'
 
+import Layout from '../layout/index.vue'
 import Index from '../views/Index.vue'
 import Team from '../views/Team.vue'
 import SearchTag from '../views/SearchTag.vue'
 import UserInfo from '../views/UserInfo.vue'
 import UserEdit from '../views/UserEdit.vue'
 import SearchResultList from '../views/SearchResultList.vue'
+import Login from '../views/login.vue'
 
 // 路由配置
 const routes = [
-  { path: '/index', title: '首页', component: Index },
-  { path: '/team', title: '队伍', component: Team },
-  { path: '/searchTag', component: SearchTag },
-  { path: '/user', title: '用户信息', component: UserInfo },
-  { path: '/user/edit', title: '用户信息修改', component: UserEdit },
-  { path: '/searchTag/searchResultList', title: '搜索结果', component: SearchResultList },
+  { path: '/login', name: 'Login', title: '登录', component: Login },
+  { path: '/', 
+    component: Layout,
+    redirect: '/index', 
+    children:[
+      { path: 'index', title: '首页', component: Index },
+    ]
+  },
+  { path: '/', component: Layout,
+    children:[
+      { path: 'team', title: '队伍', component: Team },
+    ]
+  },
+  { path: '/', component: Layout,
+    children:[
+      { path: 'searchTag', component: SearchTag },
+      { path: 'searchTag/searchResultList', title: '搜索结果', component: SearchResultList },
+    ]
+  },
+  { path: '/', component: Layout,
+    children:[
+      { path: 'user', title: '用户信息', component: UserInfo },
+      { path: 'user/edit', title: '用户信息修改', component: UserEdit }
+    ]
+  },
 ]
 
 // 创建路由实例
